72c035f8b43fac8ffc9007a70b918246565c0188
Jeffreykerley <jakerley180@gmail.com>
2022-04-12 23:47:34 -0500

    hw7

--- StatModelOne/Stats_HW7/stat_hw7_s.R
+++ StatModelOne/Stats_HW7/stat_hw7_s.R
@@ -57,4 +57,42 @@ anova(p1,p2,p3)
 
 #b 
 
+accRange = range(Auto$acceleration)
+accGrid = seq(from = accRange[1], to = accRange[2])
+
+splineC3 = lm(horsepower ~ ns(acceleration, df=4), data=Auto)
+pAccRange = predict(splineC3, newdata = list(acceleration = accGrid), se = T)
+
+plot(acceleration, horsepower, col="black")
+lines(accGrid, pAccRange$fit, lwd=2) 
+lines(accGrid, pAccRange$fit+2*pAccRange$se, lty="dashed") 
+lines(accGrid, pAccRange$fit-2*pAccRange$se, lty="dashed") 
+
+#c 
+plot(acceleration, horsepower, col="black")
+smoothS = smooth.spline(acceleration, horsepower, cv = TRUE)
+smoothS$df
+lines(smoothS,col="blue",lwd=2)
+
+#d 
+plot(acceleration, horsepower, xlim=accRange, col="red")
+LR2=loess(horsepower ~ acceleration, span=.2, data=Auto) 
+LR5=loess(horsepower ~ acceleration, span=.5, data=Auto)
+
+lines(accGrid, predict(LR2, data.frame(acceleration=accGrid)), col="green", lwd=2)
+lines(accGrid, predict(LR5, data.frame(acceleration=accGrid)), col="blue", lwd=2)
+
+LR2
+LR5
+
+#e and f
+gam1 = lm(horsepower ~  ns(acceleration, 5), data = Auto)
+gam2 = lm(horsepower ~ ns(weight) + ns(acceleration, 5), data = Auto)
+gam3 = lm(horsepower ~ ns(weight, 4) + ns(acceleration, 5), data = Auto)
+gam1
+gam2
+gam3
+anova(gam1,gam2,gam3)
+
+
 
